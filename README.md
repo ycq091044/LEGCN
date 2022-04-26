145e8b6c68c9b7543c28e4df41bcb6fb556b3aa4
ycq091044 <ycqsjtu@gmail.com>
2022-04-26 14:30:11 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Hypergraph Learning with Line Expansion
+
+This is a very interesting paper finished on Feb. 2020 when I was in my previous group. Unfortunately, I have been so reluctant that I cannot find a chance to invest time for further improvements and re-submission. Until now, I decide to let it go. **This paper utilizes an elegant hypergraph transformation (i.e., line expansion) to enable all existing graph learning algorithms to work effortlessly on hypergraphs.**
+
+### Package Dependency
+The package dependency is light: ``pytorch``, ``scipy``, ``numpy``, compatible with recent versions.
+
+### Code Structure
+- ```data/```: under this folder, we provide several graph, hypergraph datasets (processed)
+    - **Hypergraphs**: 20newsW100, ModelNet40, Mushroom, NTU2012, zoo
+    - **graphs**: cora, citeseer, pubmed
+- ```src/```
+    - **layers.py**: standard neural network layers
+    - **models.py**: GCN, GAT, SpGAT (sparse GAT model)
+    - **utils.py**: other auxiliary functions
+    - **train.py**: the running script
+- ```config/```: containing the hyperparameter configurations
+
+### Recommended Setting
+python train.py --dataset 20newsW100  --LE 1 --weight_l2 1e-4 --lr 0.001 --dropout 0.5
+python train.py --dataset Mushroom --hidden 128 --LE 1 --lr 0.01 --weight_l2 1e-3
+python train.py --dataset NTU2012 --LE 1 --lr 0.01 --hidden 128
+python train.py --dataset ModelNet40 --lr 0.01 --hidden 128 --LE 1
+python train.py --dataset zoo --LE 1 --hidden 32 --dropout 0.5 --lr 0.02
+
+### Citation
+If you think this is repo useful, please cite our paper. For question answering, please contact chaoqiy2@illinois.edu.
+```bibtex
+@article{yang2020hypergraph,
+  title={Hypergraph learning with line expansion},
+  author={Yang, Chaoqi and Wang, Ruijie and Yao, Shuochao and Abdelzaher, Tarek},
+  journal={arXiv preprint arXiv:2005.04843},
+  year={2020}
+}
+```
---
+++ code_and_data/src/train 2.py
@@ -0,0 +1,147 @@
+import time
+import argparse
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+from utils import load_data, accuracy, sparse_mx_to_torch_sparse_tensor
+from models import GCN, SpGAT, GAT
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--seed', type=int, default=200, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.002,
+                    help='Initial learning rate.')
+parser.add_argument('--fastmode', type=int, default=0,
+                    help='Validate during training pass.')
+parser.add_argument('--weight_l2', type=float, default=1.5e-3,
+                    help='weight for parameter L2 regularization')
+parser.add_argument('--weight_decay', type=float, default=5e-3,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=32,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.5,
+                    help='Dropout rate (1 - keep probability).')
+parser.add_argument('--modelType', type=int, default=0,
+                    help='GCN (0), SpGAT (1), GAT (2)')
+parser.add_argument('--LE', type=int, default=1,
+                    help='on graphs (0), on line expansion (1)')
+parser.add_argument('--dataset', type=str, default='cora',
+                    help='Name of dataset')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# Load data
+if args.LE == 0:
+    adj, features, labels, idx_train, idx_val, idx_test = load_data(0, dataset=args.dataset)
+else:
+    adj, Pvp, features, labels, idx_train, idx_val, idx_test = load_data(1, dataset=args.dataset)
+
+if args.cuda:
+    features = features.cuda()
+    adj = adj.cuda()
+    if args.LE == 1:
+        Pvp = Pvp.cuda()
+    labels = labels.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+def train(model, epoch):
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    if args.LE == 1:
+        output = model(features, adj, args.LE, Pvp)
+    else:
+        output = model(features, adj, args.LE)
+    loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+
+    l2 = 0
+    for p in model.parameters():
+        l2 = l2 + (p ** 2).sum()
+    loss_train = loss_train + args.weight_l2 * l2
+
+    acc_train = accuracy(output[idx_train], labels[idx_train])
+    loss_train.backward()
+    optimizer.step()
+
+    if not args.fastmode:
+        # Evaluate validation set performance separately,
+        # deactivates dropout during validation run.
+        model.eval()
+        if args.LE == 1:
+            output = model(features, adj, args.LE, Pvp)
+        else:
+            output = model(features, adj, args.LE)
+
+    loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+    acc_val = accuracy(output[idx_val], labels[idx_val])
+    print('Epoch: {:04d}'.format(epoch+1),
+          'loss_train: {:.4f}'.format(loss_train.item()),
+          'acc_train: {:.4f}'.format(acc_train.item()),
+          'loss_val: {:.4f}'.format(loss_val.item()),
+          'acc_val: {:.4f}'.format(acc_val.item()),
+          'time: {:.4f}s'.format(time.time() - t), flush=True)
+
+def test(model):
+    model.eval()
+    if args.LE == 1:
+        output = model(features, adj, args.LE, Pvp)
+    else:
+        output = model(features, adj, args.LE)
+    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.item()),
+          "accuracy= {:.4f}".format(acc_test.item()))
+    return acc_test.item()
+
+
+# model definition
+if args.modelType == 0:
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+    model = GCN(nfeat=features.shape[1],
+                nhid=args.hidden,
+                nclass=labels.max().item() + 1,
+                dropout=args.dropout)   
+elif args.modelType == 1:
+    adj = torch.FloatTensor(np.array(adj.todense()))
+    model = SpGAT(nfeat=features.shape[1],
+                nhid=args.hidden,
+                nclass=labels.max().item() + 1,
+                dropout=args.dropout)
+elif args.modelType == 2:
+    adj = torch.FloatTensor(np.array(adj.todense()))
+    model = GAT(nfeat=features.shape[1],
+                nhid=args.hidden,
+                nclass=labels.max().item() + 1,
+                dropout=args.dropout)
+
+if args.cuda:
+    model.cuda()
+optimizer = optim.Adam(model.parameters(),
+            lr=args.lr, weight_decay=args.weight_decay)
+
+# train model
+t_total = time.time()
+for epoch in range(args.epochs):
+    train(model, epoch)
+print("Optimization Finished!")
+print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
+#test model
+test(model)
